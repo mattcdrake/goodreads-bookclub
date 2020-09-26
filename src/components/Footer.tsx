a9e7d0b4e02dc4467c92d70c23a34057a5f5edd8
Matt Drake <drake.mattc@gmail.com>
2020-09-26 02:33:23 -0400

    Finish ClubPage draft

---
+++ src/components/Footer.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+//import { Link } from "react-router-dom";
+
+function Footer() {
+  return (
+    <div className="text-white bg-gray-600 px-8">Support us on Patreon!</div>
+  );
+}
+
+export { Footer };
